5c6b1410dd00210b81972e07d73e000186a01953
Zartab M Nakhwa <zartab@codewithz.com>
2026-05-21 07:59:40 +0530

    Base Project

---
+++ src/main/java/com/beacon/BeaconIngestionApplication.java
@@ -0,0 +1,13 @@
+package com.beacon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeaconIngestionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeaconIngestionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/beacon/BeaconIngestionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beacon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeaconIngestionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
